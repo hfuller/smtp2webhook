92873da01cfaec8f3d2c571df38f1b5dae90e627
Hunter Fuller <hfuller@pixilic.com>
2024-04-01 22:13:58 -0500

    Add pushover support
    This also means we look up the service's schema from its env var (servicename_schema). Currently it has to be pushover or discord.

--- lambda_function.py
+++ lambda_function.py
@@ -2,6 +2,38 @@ import json
 import urllib.request
 import os
 
+def notify(service, push_from, push_subject, push_body):
+    schema = os.environ.get(service + "_schema")
+    target = os.environ.get(service + "_target")
+    if target is None:
+        notify_discord(os.environ.get("test_target"), "AWS Lambda", "Target url not defined for service", service)
+    else:
+        if schema == "discord":
+            notify_discord(target, push_from, push_subject, push_body)
+        elif schema == "pushover":
+            notify_pushover(target, push_from, push_subject, push_body)
+        else:
+            notify_discord(os.environ.get("test_target"), "AWS Lambda", "Schema not defined for service", service)
+
+def notify_pushover(user, push_from, push_subject, push_body):
+    new_subject = push_from + " - " + push_subject
+    if push_body == "":
+        push_body = "(no message was provided)"
+    data = {
+        "token": os.environ.get("config_token_pushover"),
+        "user": user,
+        "title": new_subject[:250],
+        "message": push_body[:1024]
+    }
+    
+    request = urllib.request.Request("https://api.pushover.net/1/messages.json", urllib.parse.urlencode(data).encode("ascii"))
+    request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    request.add_header('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686) Gecko/20071127 Firefox/2.0.0.11 PixilicSESWebhookLambda/1')
+    
+    response = urllib.request.urlopen(request)
+    print(response.read())
+    
+
 def notify_discord(url, push_from, push_subject, push_body):
     #for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
     data = {
@@ -37,28 +69,24 @@ def lambda_handler(event, context):
         if record["eventSource"] == "aws:ses":
             m_subject = record["ses"]["mail"]["commonHeaders"]["subject"]
             m_from = record["ses"]["mail"]["commonHeaders"]["from"][0] #why tf is this an array
+            #no bodies if we got it via ses directly
+            
             for destination in record["ses"]["mail"]["destination"]:
                 service = destination.split('@')[0].lower()
                 print("Found service", service)
-                url = os.environ.get(service + "_target")
-                if url is None:
-                    notify_discord(os.environ.get("test_target"), "AWS Lambda", "Target url not defined for service", service)
-                else:
-                    notify_discord(url, m_from, m_subject, "")
+                #REAL NOTIFICATION
+                notify(service, m_from, m_subject, "")
         elif record["eventSource"] == "aws:sns":
             sns_contents = json.loads(record["Sns"]["Message"])
             m_subject = sns_contents["mail"]["commonHeaders"]["subject"]
             m_from = sns_contents["mail"]["commonHeaders"]["from"][0] #why tf is this an array
             m_body = sns_contents["content"].split('\n\n')[1]
-            
+
             for destination in sns_contents["mail"]["destination"]:
                 service = destination.split('@')[0].lower()
                 print("Found service", service)
-                url = os.environ.get(service + "_target")
-                if url is None:
-                    notify_discord(os.environ.get("test_target"), "AWS Lambda", "Target url not defined for service", service)
-                else:
-                    notify_discord(url, m_from, m_subject, m_body)
+                #REAL NOTIFICATION
+                notify(service, m_from, m_subject, m_body)
         else:
             notify_discord(os.environ.get("test_target"), "AWS Lambda", "Unknown eventSource", record["eventSource"])
             continue
